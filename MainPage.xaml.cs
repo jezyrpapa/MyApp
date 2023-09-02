bd3b03b102dfb43e6956b6301d48e84c9c6c863c
USER <USER@DESKTOP-RJOAGFE>
2023-09-02 13:20:37 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MyFirstApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            btn1.Text = "10";
+            btn10.Text = "1";
+
+        }
+    }
+}
